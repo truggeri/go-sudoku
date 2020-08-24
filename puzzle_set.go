f404463081ef94a2daadff5182b4aeb14c181ccf
Thomas Ruggeri <truggeri@gmail.com>
2020-08-24 18:20:10 -0400

    Adding method to PuzzleSet for possibilities
    Gives a bool array of all the possible values in the set

--- puzzle_set.go
+++ puzzle_set.go
@@ -1,4 +1,33 @@
 package main
 
-// PuzzleSet One linewidth of squares
+// PuzzleSet One linewidth of puzzle squares
+// Can represent a row, column or cube
 type PuzzleSet [lineWidth]PuzzleSquare
+
+// Possibilities Gives which numbers are possible in the set
+// @param exclude [int] Index of element to exclude from calculation
+func (set PuzzleSet) Possibilities(exclude int) [lineWidth]bool {
+	var possibilities [lineWidth]bool
+
+	for i, element := range set {
+		if i == exclude {
+			continue
+		}
+
+		for n := range possibilities {
+			if possibilities[n] {
+				continue
+			}
+
+			if element.solved() {
+				if element.value == n+1 {
+					possibilities[n] = true
+				}
+			} else if element.possibilities[n] {
+				possibilities[n] = true
+			}
+		}
+	}
+
+	return possibilities
+}
---
+++ puzzle_set_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSetPossibilities(t *testing.T) {
+	puzzle := CreateTestPuzzle()
+	puzzle = puzzle.UpdatePossibilites()
+	var set PuzzleSet = puzzle[0]
+
+	tables := []struct {
+		index    int
+		possible bool
+	}{
+		{0, true},
+		{1, true},
+		{2, true},
+		{3, true},
+		{4, true},
+		{5, true},
+		{6, true},
+		{7, true},
+		{8, true},
+	}
+
+	poss := set.Possibilities(9)
+	for _, table := range tables {
+		if poss[table.index] != table.possible {
+			t.Errorf("Possibilities at index %d wrong, expected: %t, got %t", table.index, table.possible, poss[table.index])
+		}
+	}
+
+	tables[5].possible = false
+	poss = set.Possibilities(0)
+	for _, table := range tables {
+		if poss[table.index] != table.possible {
+			t.Errorf("Possibilities at index %d wrong, expected: %t, got %t", table.index, table.possible, poss[table.index])
+		}
+	}
+
+}
